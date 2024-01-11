ae7d5c5479f17c4c3bbf98936e80a1e71039c5a6
hijas <hijasahamed2000@gmail.com>
2024-01-11 10:25:52 +0530

    first upload

---
+++ bin/foreach.dart
@@ -0,0 +1,8 @@
+// for each syntax and working 
+// sum of the array using for each?
+void main(){
+  dynamic sum=0;
+  List arr=[10,20,30,40,50];
+  arr.forEach((element) => sum+=element);
+  print(sum);
+}
---
+++ bin/forinloop.dart
@@ -0,0 +1,9 @@
+// find sum of the elements in the array?
+void main(){
+  List arr=[1,2,3,4,5];
+  dynamic sum=0;
+  for(var v in arr){
+    sum=sum+v;
+  }
+  print(sum);
+}
