1631f9b605480fb0f4a07b5718a192ba8797bb5c
hijas <hijasahamed2000@gmail.com>
2024-01-11 10:50:17 +0530

    last push

---
+++ bin/parameterstype.dart
@@ -0,0 +1,29 @@
+void main(){
+  requiredpara(name: 'Hijas');
+  namedpara(age: 24);
+  positional('Malappuram', 673637);
+  optionalpara('good morning');
+  defaultpara(age: 34);
+}
+
+requiredpara({required String name}){
+ print(name);
+}
+
+namedpara({int? age}){
+ print(age);
+}
+
+positional(String place,int? pincode){
+  print(place);
+  print(pincode);
+}
+
+optionalpara([String? word]){
+ print(word);
+ print('no word');
+}
+
+defaultpara({int age=24}){
+ print(age);
+}
