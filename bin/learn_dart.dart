e171fbcd36cb5ae795202a9e64e8a0ec0bfc1d2b
hijas <hijasahamed2000@gmail.com>
2024-01-15 10:47:52 +0530

    latest

---
+++ bin/learn_dart.dart
@@ -0,0 +1,13 @@
+void main(){
+  // var datatype can change its value but cannot change its datatype. 
+  var a=10;
+  print(a);
+  a=20;
+  print(a);
+
+  //dynamic datatype can change its value and datatype also.
+  dynamic z=1000;
+  print(z);
+  z='hijas';
+  print(z);
+}
