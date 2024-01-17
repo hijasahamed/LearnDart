82917355992c53a1f5fbdcb7b51adb36263a511a
hijas <hijasahamed2000@gmail.com>
2024-01-17 20:24:07 +0530

    latest

--- bin/learn_dart.dart
+++ bin/learn_dart.dart
@@ -1,17 +1,3 @@
-import 'dart:async';
 
-main(){
-  final StreamController<int> controller=StreamController<int>();
 
-  final Stream<int> stream = controller.stream;
 
-  final StreamSubscription<int> subscription = stream.listen((int data) {
-    print('Received data: $data');
-  });
-
-  controller.add(1);
-  controller.add(2);
-  controller.add(3);
-
-  subscription.cancel();
-}
---
+++ bin/mapoperation.dart
@@ -0,0 +1,8 @@
+void main(){
+  Map<String,dynamic> a={
+    'name':'hijas',
+    'address':'pulikkal'
+  };
+  print(a.keys);
+  print(a.values);
+}
