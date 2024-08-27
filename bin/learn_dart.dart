cdff6a27f2c5991f9bec9ffd1d5c8e8f429e1902
hijas <hijasahamed2000@gmail.com>
2024-08-27 18:10:17 +0530

    latest update push

--- bin/learn_dart.dart
+++ bin/learn_dart.dart
@@ -1,15 +1,5 @@
 void main(){
-  List<int> list1 = [1, 2, 3, 4, 5];
-  List<int> list2 = [3, 4, 5, 6, 7];
-
-  List res = [];
-
-  for(int i in list1){
-    if(list2.contains(i)){
-      res.add(i);
-    }
-  }
-
-  print(res);
-
+  String str = 'hijas';
+  String chk = str.split('').reversed.join();
+  print((str == chk)? 'true' : 'false');
 }
