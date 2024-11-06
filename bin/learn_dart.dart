18d1402c34e4380e12cc71ac10cfd224c8cf4f75
hijas <hijasahamed2000@gmail.com>
2024-11-06 15:04:47 +0530

    latest push

--- bin/learn_dart.dart
+++ bin/learn_dart.dart
@@ -1,9 +1,7 @@
 
 void main()async{
-  const name = 'hijas';
-  display(name);
-}
-
-display (name){
- print(name);
+  Stream<int> numberStream = Stream.periodic(Duration(seconds: 1),(computationCount) => computationCount+1,).take(5);
+  numberStream.listen((event) {
+    print(event);
+  },).onDone(() => print('Completed'),);
 }
